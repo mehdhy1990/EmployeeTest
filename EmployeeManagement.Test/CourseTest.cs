c348cc8292cd85681fe75fcf79a9b2eb5c78af6d
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-27 22:53:12 +0100

    begin

---
+++ EmployeeManagement.Test/CourseTest.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using Xunit;
+
+namespace EmployeeManagement.Test;
+
+public class CourseTest
+{
+    [Fact]
+    public void ConstructCourse_CourseConstructor_IsNewMustBeTrue()
+    {
+        //arrange
+        //nothing to arrange
+        
+        //act
+        var course = new Course("disaster management 101");
+
+        //assert
+        Assert.True(course.IsNew);
+    }
+}
